bb228d3655d3605efc92a1f28d5bffd5c81e08ba
Gabriel Torres Bolognani <torresnatela@hotmail.com>
2019-11-24 01:45:23 -0300

    testes

--- tests/testes.cpp
+++ tests/testes.cpp
@@ -13,6 +13,7 @@
 #include <math.h>
 
 Administrador *SessaoAdmin;
+Gerente *SessaoGerente;
 /*Listas vazias para testes*/
 ListaGerentes listaG;
 ListaAdmins listaA;
@@ -59,18 +60,22 @@ TEST_CASE("02 - Teste getters e setters Administrador"){
     CHECK(Admin2.get_nome()=="Gabriel");
     CHECK(Admin2.get_senha()=="14072000");  
 }
+/*------------------------------------------------------------------------------------------------------------------------------------------------*/
 
 /*----------------- Testes funcoes e construtores Gerente -------------------*/
-
+//teste construtores
 TEST_CASE("01 - Teste Construtores Gerente"){
     CHECK_NOTHROW(Gerente gerente1("Luis","0000000","1234567","12345","rua teste","31992312342"));
     CHECK_NOTHROW(Gerente gerente2("Luas", "14234533221", "mjs","122","rua teste 2","31993410213"));
 }
+
+//teste getters e setters
 TEST_CASE("02 - Teste getters e setters Gerente"){
     Gerente *gerente1 = new Gerente("teste1","teste","123456789","1234","rua teste","31999999999");
     Gerente gerente2("teste2","teste","1234567","123456","rua teste2","3188888888");
     Gerente *gerente3 = new Gerente("teste3","teste","11111111111","1234","rua local","31993410214");
 
+    //getters
     CHECK(gerente1->get_cpf() == "teste");
     CHECK(gerente2.get_cpf()== "teste");
 
@@ -89,6 +94,7 @@ TEST_CASE("02 - Teste getters e setters Gerente"){
     CHECK(gerente2.get_telefone()== "3188888888");
     CHECK(gerente3->get_telefone()=="31993410214");
 
+    //setters
     gerente1->set_cpf("01234");
     gerente2.set_endereco("testeSet");
     gerente3->set_senha("{/*pause*/}");
@@ -97,6 +103,8 @@ TEST_CASE("02 - Teste getters e setters Gerente"){
     CHECK(gerente2.get_endereco() == "testeSet");
     CHECK(gerente3->get_senha()=="{/*pause*/}");
 }
+/*--------------------------------------------------------------------------------------------------------------------------------------*/
+
 /*----------------- Teste Funcoes e construtor creche ---------------*/
 TEST_CASE("01 - Teste Construtores"){
     CHECK_NOTHROW(Creche creche1("teste","1213545345","rua teste","ferwef "" fwefwe",3));
@@ -107,7 +115,8 @@ TEST_CASE("02 - Teste getters e setters"){
     Creche creche1("teste","1213545345","rua teste","ferwef",3);
     Creche creche2("teste2","0000000","rua teste2","1231242",5);
     Creche *creche3 = new Creche("teste3","123456","rua creche","123456",100);
-
+    
+    //getters
     CHECK(creche1.get_endereco()=="rua teste");
     CHECK(creche1.get_nome()=="teste");
     CHECK(creche1.get_pos_gerente() == 3);
@@ -115,14 +124,22 @@ TEST_CASE("02 - Teste getters e setters"){
     CHECK(creche3->get_validade_convenio()=="123456");
     CHECK(creche3->get_telefone() == "123456");
 
+    //setters
 
+    creche3->set_nome("new name");
+    creche2.set_telefone("1234567");
+    creche1.set_validade_convenio("23/04/2000");
+
+    CHECK(creche3->get_nome()=="new name");
+    CHECK(creche2.get_telefone()=="1234567");
+    CHECK(creche1.get_validade_convenio()=="23/04/2000");
 
 }
 
-
+/*--------------------------------------------------------------------------------------------------------------------------------------------------*/
 /*--------------- testando lista --------------------*/
 
-TEST_CASE("Teste inserir na lista"){
+TEST_CASE("01 - Teste inserir na lista e get_elemento e remover elemento"){
     Administrador admin1("teste1","1234567","1234567");
     Creche creche1("creche1","1234567","1234567","12312423",1);
     Gerente gerente1("gerente1","1234567","1234567","000","000","000");
@@ -174,6 +191,4 @@ TEST_CASE("Teste inserir na lista"){
     CHECK(listaG.tamanho() == 1);
     CHECK(listaC.tamanho() == 2);
 }
-
-
-
+/*------------------------------------------------------------------------------------*/
