cd1384ce81a70434607969db4042d9b0c1dc0dba
Victor Vieira de Melo <victorvmelo@gmail.com>
2019-11-24 05:01:00 -0300

    Corrigindo o ultimo commit

--- include/creche.hpp
+++ include/creche.hpp
@@ -11,6 +11,13 @@ class Creche {
         std::string endereco;
         std::string validade_convenio;
         int pos_gerente;
+        int numero_de_turmas;
+        int numero_de_professores;
+        int numero_de_coordenadores;
+        int numero_de_auxiliares_bercario;
+        float valor_per_capta_aluno;
+        float valor_manuntencao;
+        float faixa_etaria_media;
     public:
 
         Creche(std::string,std::string,std::string,std::string, int);
@@ -21,6 +28,13 @@ class Creche {
         std::string get_endereco();
         std::string get_validade_convenio();
         int get_pos_gerente();
+        int get_numero_turmas();
+        int get_numero_professores();
+        int get_numero_coordenadores();
+        int get_numero_aux_berc();
+        float get_valor_per_capta();
+        float get_valor_manuntencao();
+        float get_faixa_etaria();
 
 
         //setters
@@ -29,6 +43,13 @@ class Creche {
         void set_endereco(std::string);
         void set_validade_convenio(std::string);
         void set_pos_gerente(int);
+        void set_numero_turmas(int);
+        void set_numero_professores(int);
+        void set_numero_coordenadores(int);
+        void set_numero_aux_berc(int);
+        void set_valor_per_capta(float);
+        void set_valor_manuntencao(float);
+        void set_faixa_etaria(float);
 };
 
 #endif
--- include/gerente.hpp
+++ include/gerente.hpp
@@ -7,13 +7,7 @@ class Gerente : public Usuario {
         std::string periodo_mandato;
         std::string endereco;
         std::string telefone;
-        int numero_de_turmas;
-        int numero_de_professores;
-        int numero_de_coordenadores;
-        int numero_de_auxiliares_bercario;
-        float valor_per_capta_aluno;
-        float valor_manuntencao;
-        float faixa_etaria_media;
+        
 
         //funcoes creche
         
@@ -37,13 +31,7 @@ class Gerente : public Usuario {
         std::string get_nome() override;
         std::string get_endereco();
         std::string get_senha() override;
-        int get_numero_turmas();
-        int get_numero_professores();
-        int get_numero_coordenadores();
-        int get_numero_aux_berc();
-        float get_valor_per_capta();
-        float get_valor_manuntencao();
-        float get_faixa_etaria();
+        
 
         //setters
         void set_periodo_mandato(std::string);
@@ -53,13 +41,6 @@ class Gerente : public Usuario {
         void set_nome(std::string) override;
         void set_senha(std::string) override;
         void Login(std::string) override;
-        void set_numero_turmas(int);
-        void set_numero_professores(int);
-        void set_numero_coordenadores(int);
-        void set_numero_aux_berc(int);
-        void set_valor_per_capta(float);
-        void set_valor_manuntencao(float);
-        void set_faixa_etaria(float);
 };
 
 
