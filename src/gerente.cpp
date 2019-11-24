0128243166b848d0b00bc19f2c9786896a4fea64
Victor Vieira de Melo <victorvmelo@gmail.com>
2019-11-24 06:10:46 -0300

    Editar Creche como gerente

--- src/gerente.cpp
+++ src/gerente.cpp
@@ -108,7 +108,72 @@ void Gerente::Login(std::string _cpf){
 }
 
 void Gerente::EditarCreche(){
+    extern Gerente SessaoGerente;
+    extern ListaGerentes listaG;
+    extern ListaCreches listaC;
+    gerentes *gerente_atual = listaG.primeiro;
+    creches *creche_atual = listaC.primeiro;
+    int checa_creche = 0;
+    int id_gerente = 0;
+    int id_creche;
+    int _numero_professores, _numero_coord, _numero_aux_berc, _numero_turmas, _numero_alunos;
+    float _per_capta, _manuntencao, _faixa_etaria;
 
+    for(int i = 0; i < listaG.tamanho(); i++){
+        if(SessaoGerente.get_cpf()==gerente_atual->gerente->get_cpf()){
+            id_gerente = i;
+        }
+        gerente_atual = gerente_atual->proximo;
+    }
+
+    for(int i = 0; i < listaC.tamanho(); i++){
+        if(creche_atual->creche->get_pos_gerente() == id_gerente){
+            checa_creche++;
+            id_creche = i;
+        }
+        creche_atual = creche_atual->proximo;
+    }
+    creche_atual = listaC.primeiro;
+
+    if(checa_creche>0){
+        Creche *creche = listaC.get_creche(id_creche);
+        std::cout << "Informe os dados de sua creche" << std::endl;
+        std::cout << "---------------------------------------------------------" << std::endl;
+        std::cout << "Informe o numero de turmas de sua instituicao:" << std::endl;
+        std::cin >> _numero_turmas;
+        std::cout << "Informe o numero de professores da sua instituicao:" << std::endl;
+        std::cin >> _numero_professores;
+        std::cout << "Informe o numero de coordenadores de sua instituicao:" << std::endl;
+        std::cin >> _numero_coord;
+        std::cout << "Informe o numero de auxiliares de bercario em sua instituicao:" << std::endl;
+        std::cin >> _numero_aux_berc;
+        std::cout << "Informe o numero de alunos em sua instituicao:" << std::endl;
+        std::cin >> _numero_alunos;
+        std::cout << "Informe o valor per capta de alunos:" << std::endl;
+        std::cin >> _per_capta;
+        std::cout << "Informe o valor de manuntencao de sua instituicao:" << std::endl;
+        std::cin >> _manuntencao;
+        std::cout << "Informe a faixa etaria media de sua instituicao:" << std::endl;
+        std::cin >> _faixa_etaria;
+
+        creche->set_numero_turmas(_numero_turmas);
+        creche->set_numero_professores(_numero_professores);
+        creche->set_numero_coordenadores(_numero_coord);
+        creche->set_numero_aux_berc(_numero_aux_berc);
+        creche->set_numero_alunos(_numero_alunos);
+        creche->set_valor_per_capta(_per_capta);
+        creche->set_valor_manuntencao(_manuntencao);
+        creche->set_faixa_etaria(_faixa_etaria);
+
+        std::cout << "Dados registrados com sucesso!" << std::endl;
+        std::this_thread::sleep_until(std::chrono::system_clock::now()+std::chrono::seconds(3));
+        SessaoGerente.Menu();
+    }
+    else{
+        std::cout << "Voce nao foi definido como gerente de nenhuma creche" << std::endl;
+        std::this_thread::sleep_until(std::chrono::system_clock::now()+std::chrono::seconds(3));
+        SessaoGerente.Menu();
+    }
 }
 
 void Gerente::EmitirRelatorio(){
@@ -141,7 +206,6 @@ int Gerente::Menu(){
                     SessaoGerente->EmitirRelatorio();
                 }
                 else if(aux_acesso == 2){
-                    system("clear");
                     SessaoGerente->EditarCreche();
                 }
                 else if(aux_acesso == 0){
@@ -187,6 +251,3 @@ void Gerente::VerDados(){
 void Gerente::EditarDados(){
     
 }
-
-
-
